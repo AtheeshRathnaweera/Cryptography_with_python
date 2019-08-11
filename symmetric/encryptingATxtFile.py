d6515cc45e920dcd00fd6048cdc96551f3ec26f8
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2019-08-11 22:37:51 +0530

    fixed the issue in decrypting function

--- symmetric/encryptingATxtFile.py
+++ symmetric/encryptingATxtFile.py
@@ -6,7 +6,7 @@ from Crypto import Random
 def encryption(fileName, outputFileName, chunkSize, key, iv):
     obj = DES3.new(key, DES3.MODE_CFB, iv)
     with open(fileName, 'r') as in_file:
-        with open(outputFileName, 'w') as out_file:
+        with open(outputFileName, 'wb') as out_file:
             while True:
                 chunk = in_file.read(chunkSize)
                 if len(chunk) == 0:
@@ -14,19 +14,20 @@ def encryption(fileName, outputFileName, chunkSize, key, iv):
                 elif len(chunk) %16 != 0:
                     chunk += ' ' * (16 - len(chunk) % 16)
                 
-                out_file.write(str(obj.encrypt(chunk))) #writing the result on an .enc file (Encode file)
+                out_file.write(obj.encrypt(chunk)) #writing the result on an .enc file (Encode file)
                 print("\n***Encrypted file created.")
 
                     
 def decryption(fileName, outputFileName, chunkSize, key, iv):
     obj = DES3.new(key, DES3.MODE_CFB, iv)
-    with open(fileName, 'r') as in_file:
+    with open(fileName, 'rb') as in_file:
         with open(outputFileName, 'w') as out_file:
             while True:
                 chunk = in_file.read(chunkSize)
                 if len(chunk) == 0:
                     break
-                out_file.write(str(obj.decrypt(chunk))) # write the result in the file
+                result = obj.decrypt(chunk) # store the decrypted chunk
+                out_file.write(result.decode("utf-8")) #decode("utf-8") use to decode bytes to string to write the result in the file
                 print("\n**Decrypted successfully!")
 
                             
@@ -41,11 +42,11 @@ def main():
 
     encryption("file.txt","file.enc",8192,key,iv) # encrypt the file and store the result in the file calles file.enc
 
-    with open("file.enc", "r") as f: # read the original file
+    with open("file.enc", "rb") as f: # read the original file
         print("\n________________ Encrypted results(file.enc)___________\n")
         print (f.read())
 
-    decryption("file.txt","res.dec",8192,key,iv) # decrypt the result file
+    decryption("file.enc","res.dec",8192,key,iv) # decrypt the result file
 
     with open("res.dec", "r") as f: # read the decr file
         print("\n________________ Decrypted results(file.dec)___________\n")
