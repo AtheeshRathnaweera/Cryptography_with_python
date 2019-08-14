3d9503b1755564973de6d48277e2249b14230646
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2019-08-14 15:16:25 +0530

    commented.

--- steganography/usingLib.py
+++ steganography/usingLib.py
@@ -1,3 +1,5 @@
+#Store a message string inside an image using python lib "cryptosteganography "
+
 from cryptosteganography import CryptoSteganography
 
 crypto_steganography = CryptoSteganography('thIsisThEseCRetKEy')#set a key 
@@ -7,4 +9,4 @@ crypto_steganography.hide('puppy.jpg', 'puppywithsecret.png', 'Hello! This is a 
 
 secret = crypto_steganography.retrieve('puppywithsecret.png') # read the .png and retrieve the message
 
-print(secret)
+print(secret) #print the secret message after reading
